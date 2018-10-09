b9444bb8c6e93fd36bcbf3f41dc33e47118e78fc
William DROZ <william.droz@idiap.ch>
2018-10-09 16:14:43 +0200

    [WIP] more rich setup

--- src/pydiarization/setup.py
+++ src/pydiarization/setup.py
@@ -1,5 +1,22 @@
 from setuptools import setup, find_packages
+
+long_description = ''
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pydiarization',
       version='0.1',
-      packages=find_packages()
-      )
+      packages=find_packages(),
+      author="William Droz",
+      author_email="william.droz@idiap.ch",
+      description="Diarization toolkit wrapper",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/wdroz/IBDiarization/tree/master/src/pydiarization",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Science/Research",
+        "Topic :: Multimedia :: Sound/Audio :: Speech"
+    ])
